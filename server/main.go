bdc832b917e116cc7c3259808b0ec959c4d3967d
rahmatadlin <rahmatadlin210@gmail.com>
2024-02-22 12:03:42 +0700

    Second Commit: Server Done

--- server/main.go
+++ server/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Todo struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Done bool `json:"done"`
-	ID int `json:"id"`
+	ID 		int 		`json:"id"`
+	Title 	string 		`json:"title"`
+	Done 	bool 		`json:"done"`
+	Body 	string 		`json:"body"`
 
 }
 
@@ -21,8 +21,45 @@ func main() {
 
 	app:= fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
