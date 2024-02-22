19c3b615f8c82100d1cb17a367b1fb2c1dd8b1d8
rahmatadlin <rahmatadlin210@gmail.com>
2024-02-22 11:42:18 +0700

    First Commit: Set Up Project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Todo-Golang-React
+
+## Initialize Go app
+go mod init github.com/rahmatadlin/Todo-Golang-React
+
+## Install Fiber v2
+go get -u github.com/gofiber/fiber/v2
+
+## Create client app with Vite
+npm create vite@latest
+
+## Install dependencies
+npm i @mantine/hooks @mantine/core swr @primer/octicons-react
---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Done bool `json:"done"`
+	ID int `json:"id"`
+
+}
+
+
+func main() {
+	fmt.Print("Hello World")
+
+	app:= fiber.New()
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
